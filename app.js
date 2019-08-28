22e2fe3a27f6867d5eb0b333db259226f25e8880
Hieu Nguyen <hieunguyen@Hieus-MacBook-Pro.local>
2019-08-27 18:49:29 -0700

    level 4 auth

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+// const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+
 const app = express();
 
 
@@ -19,7 +24,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields:["password"]});
+// userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields:["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -35,9 +40,11 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+  // Store hash in your password DB.
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: hash
   });
   newUser.save(function(err){
     if(err){
@@ -47,6 +54,7 @@ app.post("/register", function(req, res){
     }
   });
 });
+});
 app.post("/login", function(req, res){
   const username = req.body.username;
   const password = req.body.password;
@@ -55,7 +63,12 @@ app.post("/login", function(req, res){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
+        if(foundUser){
+          bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result === true){
+              res.render("true");
+            }
+          });
           res.render("secrets");
         }
         else{
